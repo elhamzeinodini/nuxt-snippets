11ebd7818f0ab30fee6fd0657359fef322312079
elhamzeinodini <elhammth@gmail.com>
2025-01-03 20:10:49 +0330

    Add restaurant menu API and page to display menu && add biome

---
+++ easybestell-b2c/pages/[restaurantCode]/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>{{ data }}</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const { restaurantCode } = route.params
+const { data, error, status: isLoading } = useFetch(`/api/b2c/food-online?rc=${restaurantCode}`)
+</script>
---
+++ easybestell-b2c/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>this is the main page</p>
+</template>
