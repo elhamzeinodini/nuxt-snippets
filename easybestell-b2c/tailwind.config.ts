cecf2e5ef5d4faa2ca54ad87a75ec0b2a2111d97
elhamzeinodini <elhammth@gmail.com>
2025-01-03 17:30:45 +0330

    init config

---
+++ easybestell-b2c/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app.vue',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
+
