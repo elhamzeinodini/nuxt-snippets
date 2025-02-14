9f69979f12855889962ca22cc7a517487b9be9c0
elhamzeinodini <elhammth@gmail.com>
2025-02-14 21:07:35 +0330

    trim files

---
+++ nuxt-starter/nuxt.config.ts
@@ -0,0 +1,32 @@
+export default defineNuxtConfig({
+  css: ["~/assets/css/main.css"],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  compatibilityDate: "2025-01-03",
+
+  modules: [
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "acceptHMRUpdate"],
+      },
+    ]
+  ],
+
+  imports: {
+    dirs: ["stores"],
+  },
+
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ]
+})
